e029bf9511a0c1c725ce1749d4adb926ba83d57a
Martins <gabrielzinnskk@gmail.com>
2026-06-24 20:35:19 -0300

    feat: identificador de tema do celular

--- App.js
+++ App.js
@@ -1,20 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState, useEffect } from 'react';
+import { Appearance, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [tema, setTema] = useState(Appearance.getColorScheme());
+
+  useEffect(() => {
+    const assinatura = Appearance.addChangeListener(({ colorScheme }) => {
+      setTema(colorScheme);
+    });
+
+    return () => assinatura.remove();
+  }, []);
+
+  const ehTemaEscuro = tema === 'dark';
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={[styles.container, ehTemaEscuro ? styles.fundoEscuro : styles.fundoClaro]}>
+      <Text style={ehTemaEscuro ? styles.textoClaro : styles.textoEscuro}>
+        Check-in de Estudos
+      </Text>
+      <Text style={ehTemaEscuro ? styles.textoClaro : styles.textoEscuro}>
+        Tema do sistema: {ehTemaEscuro ? 'Escuro' : 'Claro'}
+      </Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  container: { flex: 1, justifyContent: 'center', alignItems: 'center', gap: 10 },
+  fundoEscuro: { backgroundColor: '#121212' },
+  fundoClaro: { backgroundColor: '#ffffff' },
+  textoClaro: { color: '#ffffff', fontSize: 16 },
+  textoEscuro: { color: '#000000', fontSize: 16 },
 });
